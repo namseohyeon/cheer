65c496c5df349ccc52e5cb0f029fdacc68449145
namseohyeon <syn5421a@gmail.com>
2022-11-11 23:55:48 +0900

    카테고리 기능 오류 수정

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Post, Comment,Category
 # Register your models here.
 admin.site.register(Post)
@@ -8,13 +7,3 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
 admin.site.register(Category, CategoryAdmin)
-=======
-from .models import Post, Category
-# Register your models here.
-admin.site.register(Post)
-
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-
-admin.site.register(Category,CategoryAdmin)
->>>>>>> 7a05a9813d913561c53a0afdd97cda6985cffd20
--- mainapp/models.py
+++ mainapp/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from account.models import User
 
 # Create your models here.
+class Tag(models.Model):
+    name=models.CharField(max_length=10)
+
+    def __str__(self) : 
+        return self.name
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -18,7 +24,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    Tag = models.IntegerField(2)
+    Tag = models.ManyToManyField(Tag,blank=True)
     img = models.ImageField(blank=True, null=True, upload_to='cheer_photo/%y/%m/%d/')
     file = models.FileField(blank=True, null=True, upload_to='cheer_file/%y/%m/%d/')
     date = models.DateTimeField(auto_now_add=True)
