e0fae453dde45d0866c0aa661788b240fccacf25
KwonMinjeong-commit <mxnjxxng@naver.com>
2022-11-14 15:35:10 +0900

    메인페이지 나브바, 레이아웃 구성

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,13 +8,13 @@ urlpatterns = [
     path('board/', views.board_post, name="board_post"),
     path('board/<int:pk>/', views.board_detail, name="board_detail"),
 
-    path('landing/', views.landing),
-
     path('comment_create/<int:pk>/', views.comment_create, name="comment_create"),
     path('comment_update/<int:pk>/', views.comment_update, name="comment_update"),
     path('comment_delete/<int:pk>/', views.comment_delete, name="comment_delete"),
 
     path('scrap/<int:pk>/', views.scrap, name="scrap"),
     # path('scrap_delete/<int:pk>/', views.scrap_delete, name="scrap_delete"),
+
+    path('landing/', views.landing),
     
 ]
